a7aee8bddaff6340026747377309a75904375918
hitesh4586 <hitesh.c.vasu@gmail.com>
2024-02-29 18:35:24 +0500

    alerts and variables

---
+++ app.js
@@ -0,0 +1,20 @@
+var Name = "Heetesh Kumar";
+var FatherName = "Chetan";
+var Surname ="Brahaman";
+var CNIC ="44303-1483143-7";
+
+alert(Name)
+document.write(Name)
+console.log(Name)
+
+alert(FatherName)
+document.write(FatherName)
+console.log(FatherName)
+
+alert(Surname)
+document.write(Surname)
+console.log(Surname)
+
+alert(CNIC)
+document.write(CNIC)
+console.log(CNIC)
