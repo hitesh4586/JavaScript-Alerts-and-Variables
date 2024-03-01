55313ad316a86932d15035d7f698dbf46f5425d7
hitesh4586 <hitesh.c.vasu@gmail.com>
2024-03-01 17:10:15 +0500

    Variables changed from Personal to Company

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
-var Name = "Heetesh Kumar";
-var FatherName ="Chetan";
-var Surname ="Brahaman";
-var CNIC ="44303-1483143-5";
+var CompanyName = "Fashion Friend";
+var Owner ="Hitesh Kumar";
+var MailingAddress ="P.O Box Mithi";
+var Email ="hitesh.c.vasu@gmail.com";
+var PhoneNumber ="03353512637";
+var Nominal ="100 Millions";
+var Industry ="Textiles";
 
-alert(Name)
-document.write(Name)
-console.log(Name)
+alert(CompanyName)
+document.write(CompanyName)
+console.log(CompanyName)
 
-alert(FatherName)
-document.write(FatherName)
-console.log(FatherName)
+alert(Owner)
+document.write(Owner)
+console.log(Owner)
 
-alert(Surname)
-document.write(Surname)
-console.log(Surname)
+alert(MailingAddress)
+document.write(MailingAddress)
+console.log(MailingAddress)
 
-alert(CNIC)
-document.write(CNIC)
-console.log(CNIC)
+alert(Email)
+document.write(Email)
+console.log(Email)
+
+alert(PhoneNumber)
+document.write(PhoneNumber)
+console.log(PhoneNumber)
+
+alert(Nominal)
+document.write(Nominal)
+console.log(Nominal)
+
+alert(Industry)
+document.write(Industry)
+console.log(Industry)
